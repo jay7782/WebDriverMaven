764cd155b46de83539f0d7bcc80eff64abf9f736
Jay Sathyanarayana <jsathyanarayana7782@gmail.com>
2018-10-08 22:09:37 -0400

    Initial Commit

---
+++ src/test/java/webdrivermaven/LoginTest.java
@@ -0,0 +1,27 @@
+package webdrivermaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setup() {
+		driver = new FirefoxDriver();
+	}
+	@Test
+	public void dologin() {
+		driver.get("http://gmail.com/");
+		driver.findElement(By.id("Email")).sendKeys("jay@gmail.com");
+		driver.findElement(By.id("Passwd")).sendKeys("sdsfgg");
+	}
+	@AfterSuite
+	public void teardown(){
+		driver.quit();
+	}
+
+}
